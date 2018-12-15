63ec94ece18856694fc431301235f8e4e6b39de5
daniel-kashin <dskashin_1@edu.hse.ru>
2018-12-15 23:10:50 +0300

    Add logging

--- src/main/kotlin/com/edoctor/api/HelloWorldController.kt
+++ src/main/kotlin/com/edoctor/api/HelloWorldController.kt
@@ -1,5 +1,6 @@
 package com.edoctor.api
 
+import mu.KotlinLogging.logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -8,11 +9,16 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloWorldController {
 
+    val log = logger { }
+
     @Autowired
     lateinit var helloWorldService: HelloWorldService
 
     @GetMapping("/")
-    fun index(@RequestParam(value = "name") name: String) = helloWorldService.getUser(name)
+    fun index(@RequestParam(value = "name") name: String) =
+            helloWorldService
+                    .getUser(name)
+                    .also { log.info { "$it" } }
 
     @GetMapping("/hello")
     fun hello(): String {
