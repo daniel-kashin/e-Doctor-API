eb787cb4dc661e7dfb54910b6f397c0c012a543c
daniel-kashin <dskashin_1@edu.hse.ru>
2019-01-10 16:39:17 +0800

    Fix compile error

--- src/main/kotlin/com/edoctor/api/entities/security/AuthorizationController.kt
+++ src/main/kotlin/com/edoctor/api/entities/security/AuthorizationController.kt
@@ -1,10 +1,7 @@
 package com.edoctor.api.entities.security
 
 import com.edoctor.api.entities.Patient
-import com.edoctor.api.entities.User
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
--- src/main/kotlin/com/edoctor/api/entities/security/SecurityConfig.kt
+++ src/main/kotlin/com/edoctor/api/entities/security/SecurityConfig.kt
@@ -1,13 +1,11 @@
 package com.edoctor.api.entities.security
 
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
-import org.springframework.http.HttpMethod
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
@@ -16,7 +14,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity
 
 @Configuration
 @EnableWebSecurity
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Autowired
     private lateinit var userDetailsService: UserDetailsRepository
