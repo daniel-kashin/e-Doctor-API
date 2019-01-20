8437d5f2d790da6e5b89a15f9a7311867ae08112
daniel-kashin <dskashin_1@edu.hse.ru>
2019-01-21 00:13:56 +0300

    Chat improvements

--- src/main/kotlin/com/edoctor/api/chat/ChatHandler.kt
+++ src/main/kotlin/com/edoctor/api/chat/ChatHandler.kt
@@ -1,13 +1,15 @@
 package com.edoctor.api.chat
 
+import com.google.gson.Gson
 import mu.KotlinLogging
-import org.codehaus.jackson.map.ObjectMapper
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.CloseStatus
 import org.springframework.web.socket.TextMessage
 import com.edoctor.api.chat.TextMessage as ChatTextMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
+import java.lang.StringBuilder
+import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class ChatHandler : TextWebSocketHandler() {
@@ -15,31 +17,61 @@ class ChatHandler : TextWebSocketHandler() {
     val log = KotlinLogging.logger { }
 
     // TODO: synchronize
-    var chatSessions: Map<String, WebSocketSession> = hashMapOf()
+    var chatSessions: Map<String, List<WebSocketSession>> = ConcurrentHashMap()
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
-        log.info { "handleTextMessage(principal=${session.principal}, $message)" }
-
         try {
-            session.sendMessageIfOpened(message)
+            log.info { "handleTextMessage(chatSessions=${chatSessionsLogInfo()})" }
 
-            val chatMessage = ObjectMapper().readValue(message.payload, ChatTextMessage::class.java)
-            chatSessions[chatMessage.recipientEmail].sendMessageIfOpened(message)
+            session.sendMessageIfOpened(message)
+            log.info { "handleTextMessage sendToSender(email=${session.email}, ${message.payload})" }
+
+            val chatMessage = Gson().fromJson(message.payload, ChatTextMessage::class.java)
+            chatSessions[chatMessage.recipientEmail]?.forEach {
+                it.sendMessageIfOpened(message)
+                log.info { "handleTextMessage sendToReceiver(email=${it.email}, ${message.payload})" }
+            }
         } catch (e: Exception) {
-            log.info { "handleTextMessage error(principal=${session.principal}: $e" }
+            log.info { "handleTextMessage error(email=${session.email}: $e" }
         }
     }
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
-        log.info { "afterConnectionEstablished(principal=${session.principal}, chatSessions=$chatSessions)" }
+        session.email?.let {
+            val list = chatSessions[it]
+            if (list == null) {
+                chatSessions += it to listOf(session)
+            } else {
+                chatSessions += it to (list + session)
+            }
+        }
 
-        session.email?.let { chatSessions += it to session }
+        log.info { "afterConnectionEstablished(email=${session.email}, chatSessions=${chatSessionsLogInfo()})" }
     }
 
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
-        log.info { "afterConnectionClosed(principal=${session.principal}, chatSessions=$chatSessions)" }
+        session.email?.let {
+            val list = chatSessions[it]
+            if (list != null) {
+                val afterRemove = list - session
+                if (afterRemove.isEmpty()) {
+                    chatSessions -= it
+                } else {
+                    chatSessions += it to afterRemove
+                }
+            }
+        }
 
-        session.email?.let { chatSessions -= it }
+        log.info { "afterConnectionClosed(email=${session.email}, chatSessions=${chatSessionsLogInfo()})" }
+    }
+
+    private fun chatSessionsLogInfo(): String {
+        val builder = StringBuilder("[")
+         chatSessions.keys.forEach {
+            builder.append((it to (chatSessions[it]?.size ?: 0)).toString())
+        }
+        builder.append("]")
+        return builder.toString()
     }
 
     private fun WebSocketSession?.sendMessageIfOpened(message: TextMessage) {
