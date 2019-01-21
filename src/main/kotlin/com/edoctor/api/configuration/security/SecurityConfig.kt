5fdd0a63ea9072dbc06d27980372108c25e980c2
daniel-kashin <dskashin_1@edu.hse.ru>
2019-01-21 21:26:56 +0300

    Remove ResourceServerConfig

--- src/main/kotlin/com/edoctor/api/configuration/security/SecurityConfig.kt
+++ src/main/kotlin/com/edoctor/api/configuration/security/SecurityConfig.kt
@@ -6,14 +6,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
+import org.springframework.core.annotation.Order
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 
+@Order(2)
 @Configuration
 @EnableWebSecurity
+@EnableResourceServer
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     @Autowired
